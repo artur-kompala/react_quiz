43568c5c6b25e5eb9c67609acd50699f6654e2ca
artur-kompala <artur.kompala@gmail.com>
2023-12-15 20:05:53 +0100

    date

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-
+import DateCounter from './DateCounter'
 import './App.css';
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const apiUrl = 'https://tomaszx.pl/materialy/dane-imdb.json';
-
-fetch(proxyUrl + apiUrl, {
-  headers: {
-    'X-Requested-With': 'XMLHttpRequest'
-  }
-})
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Błąd pobierania danych:', error);
-  });
-
 
 function App() {
   
   return (
     <div className="App">
-      
+        <DateCounter></DateCounter>
     </div>
   );
 }
